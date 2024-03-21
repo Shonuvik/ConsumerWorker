f368f108a712465b478205252fd76008b20ccac3
araujo.marlon@hotmail.com <araujo.marlon@hotmail.com>
2024-03-20 23:01:46 -0300

    rabbitMQ implementing

---
+++ ConsumerFiapWorker/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ConsumerFiapWorker.Models
+{
+	public class User
+	{
+        public string UserName { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+    }
+}
+
--- ConsumerFiapWorker/Worker.cs
+++ ConsumerFiapWorker/Worker.cs
@@ -1,4 +1,10 @@
-﻿namespace ConsumerFiapWorker;
+﻿using ConsumerFiapWorker.Models;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+namespace ConsumerFiapWorker;
 
 public class Worker : BackgroundService
 {
@@ -13,8 +19,32 @@ public class Worker : BackgroundService
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            using var connection = connectionFactory.CreateConnection();
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("queue", false, false, false, null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (sender, eventArgs) =>
+                {
+                    var body = eventArgs.Body.ToArray();
+                    var json = Encoding.UTF8.GetString(body);
+                    var jsonToObject = JsonSerializer.Deserialize<User>(json) ?? new User();
+
+                    Console.WriteLine(jsonToObject.Email + "-" + jsonToObject.UserName);
+                };
+
+                channel.BasicConsume("fila", true, consumer);
+            }
+
+            await Task.Delay(10000, stoppingToken);
         }
     }
 }
